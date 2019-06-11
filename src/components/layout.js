bbe1daa57a0f7da771a9cce90576a137de5d981a
Hondo <hi@hondo.co>
2019-06-11 10:43:45 -0400

    Add pride banner

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,8 +23,24 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <div class="max-w-2xl mx-auto font-sans">
-          <div class="px-4">
+        <svg viewBox='0 0 149 150' xmlns='http://www.w3.org/2000/svg' id='pridecodes-code-corner-svg' style={{ position: 'fixed', zIndex: 9999, top: 0, borderWidth: 0, borderColor: 'black', borderStyle: 'solid', right: 0 }}>
+          <g id='random' fill='none' fillRule='evenodd'>
+            <g id='code-Corner' transform='translate(-1 -75)'>
+              <g id='code' transform='rotate(-45 91.71 37.927)'>
+                <polygon id='Rectangle-2' fill='#D3555C' points='1.35355339 4.26325641e-14 36.7088924 35.3553391 1 35.4123828' />
+                <polygon id='Rectangle-2-Copy' fill='#F48D3A' points='1 35.4123828 36.7067649 35.3553391 72.0599976 70.7106781 1 70.8223826' />
+                <polygon id='Rectangle-2-Copy-2' fill='#FDC753' points='1 70.8247655 72.0642315 70.7106781 106.712464 105.35891 1 106.237148' />
+                <polygon id='Rectangle-2-Copy-3' fill='#70BC53' points='1 106.237148 106.712464 105.35891 69.9429112 142.128463 1 141.649531' />
+                <polygon id='Rectangle-2-Copy-4' fill='#249CD5' points='1 141.649531 70.6500179 141.421356 34.5875721 177.483802 1 177.061914' />
+                <polygon id='Rectangle-2-Copy-5' fill='#9D61A4' points='1 177.061914 35.2946789 176.776695 0.646446609 211.424928' />
+              </g>
+              <path d='M88.263135,101.933356 L88.263135,114.80691 L82.5586862,114.80691 L82.5586862,96.2289077 L101.136688,96.2289077 L101.175232,101.894813 L94.6228245,101.894813 L88.263135,101.933356 Z M124.879897,108.666002 L129.505126,113.29123 L89.9208764,130.134772 L85.2956476,125.509543 L124.879897,108.666002 Z M124.145428,137.815649 L124.183971,131.45596 L124.183971,124.903552 L129.849877,124.942096 L129.849877,143.520098 L111.271874,143.520098 L111.271874,137.815649 L124.145428,137.815649 Z'
+              id='Overlay' fill='#FFF' fillRule='nonzero' />
+            </g>
+          </g>
+        </svg>
+        <div className="max-w-2xl mx-auto font-sans">
+          <div className="px-4">
             <main>{children}</main>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1 class="leading-none text-5xl text-gray-900">CT Digital Service</h1>
-    <div class="leading-normal text-xl text-gray-700">
+    <h1 className="leading-none text-5xl text-gray-900">CT Digital Service</h1>
+    <div className="leading-normal text-xl text-gray-700">
       <p>CT Digital is an open collaborative team with a vision of building and delivering better services for the people of Connecticut. We are passionate about transforming the way government works and bringing human-centered design, data-driven decision making, and an agile performance-orientation to how we work. We aim to radically change how we work to make government work better and provide more meaningful interactions for the people of Connecticut. We believe in working in the open, in listening before doing, and flipping the model of government on its head. Citizens shouldn’t have to understand how government works in order to work with government.</p>
       <p>We aim to provide a meaningful experience for citizens and businesses that lowers the state’s cost to provide services and reduces time for citizens and businesses to accomplish what they want.</p>
-      <p>If you want to learn more about what we're working on, or to work with us, send us an email <Link href="mailto:helloctdigital@ct.gov">helloctdigital@ct.gov</Link>.</p>
+      <p>If you want to learn more about what we're working on, or to work with us, send us an email <a href="mailto:helloctdigital@ct.gov">helloctdigital@ct.gov</a>.</p>
     </div>
   </Layout>
 )
